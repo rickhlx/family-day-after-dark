c0ca4580414f838bc9a6fa68ce3f0f2fff08aef9
Ricardo Herrera <rickhl@outlook.com>
2023-03-17 17:28:47 -0400

    update info on party

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,16 @@ export default function IndexPage() {
             <div class="text-xl font-serif text-gray-200 ">Welcome to</div>
             <div class="text-8xl md:text-9xl font-parklane text-amber-500">Family Day After Dark</div>
           </div>
-          <div class="flex flex-wrap h-screen content-center text-gray-200 font-serif font-semibold">
-            <p>Welcome to Family Day After Dark, a speakeasy-themed event that will transport you back to the prohibition era. Get ready for a night of cocktails, live music, and entertainment in a secret location that only the lucky few will know.</p>
+          <div class="flex flex-wrap h-screen content-center text-gray-200 font-serif font-light">
+            <p>Welcome to Family Day After Dark, a speakeasy-themed event that will transport you back to the prohibition era. Get ready for a night of cocktails, live music, and entertainment in a secret location that only few will know.</p>
             <br/>
-            <p>Step into our hidden venue and immerse yourself in the ambiance of the roaring twenties. Sip on classic cocktails, tap your toes to jazz and swing, and mingle with other guests. Remember, this is a secret event, so keep your ears open for the latest rumors to find out how to join the party. Get ready for an unforgettable night of 1920s-style excitement at Family Day After Dark.</p>
+            <p>Step into our hidden venue and immerse yourself in the ambiance of the roaring twenties. Sip on classic cocktails, tap your toes to jazz and swing, and mingle with other guests. Remember, this is a secret event, so keep your ears open for the latest rumors to find out how to join the party.</p>
+            <br/>
+            <p>Please note that this is an invite-only event, and each guest will need a ticket to gain entry. 
+              We have limited capacity, so make sure to secure your ticket early. Your ticket will also include two drink tokens, 
+              allowing you to order up to two cocktails during the event.</p>
+              <br/>
+            <p>Get ready for an unforgettable night of 1920s-style excitement at Family Day After Dark.</p>
           </div>
         </div>
       </main>
