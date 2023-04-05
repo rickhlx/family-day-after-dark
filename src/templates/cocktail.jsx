582d1bef8ce1b589ef7f72285dcd6eafaf97801b
Ricardo Herrera <rickhl@outlook.com>
2023-04-05 14:46:40 -0400

    edit cocktail card style

--- src/templates/cocktail.jsx
+++ src/templates/cocktail.jsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 
-export default function Cocktail({title, ingredients, description}) {
-    return(
-        <div className='text-gray-200 py-5'>
-          <h2 className='text-lg'>{title}</h2>
-          <h3 className='text-sm'>
-            {ingredients}
-          </h3>
-          <p className='text-sm pt-3'>
-            {description}
-          </p>
-        </div>
-    )
+export default function Cocktail({ title, origin, ingredients, description }) {
+  return (
+    <div className='text-gray-200 py-5'>
+      <div className='flex flex-col justify-between place-content-center py-5'>
+        <h2 className='basis-1/2 text-lg font-serif font-bold'>{title}</h2>
+        <h3 className='basis-1/2 font-serif italic text-xs text-amber-500'>
+          {origin}
+        </h3>
+      </div>
+      <h3 className='text-xs font-sans italic'>{ingredients}</h3>
+      <p className='text-sm pt-4 font-serif text-gray-400'>{description}</p>
+    </div>
+  );
 }
